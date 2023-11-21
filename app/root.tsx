63c0e8dc2a0082d0c208a5ed27bf05d09d4da496
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2023-11-21 14:14:15 +0100

    feat: added icons & manifest

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction } from "@remix-run/cloudflare";
+import type { LinksFunction, MetaFunction } from "@remix-run/cloudflare";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import {
   Links,
@@ -11,8 +11,28 @@ import {
 import styles from "~/styles/_base.css";
 
 export const links: LinksFunction = () => [
+  { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/favicon-32x32.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/favicon-16x16.png",
+  },
+  { rel: "manifest", href: "/manifest.webmanifest" },
+  { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#ef7b6c" },
   { rel: "stylesheet", href: styles },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+];
+
+export const meta: MetaFunction = () => [
+  { name: "msapplication-TileColor", content: "#f7f3e3" },
+  { name: "theme-color", content: "#f7f3e3" },
 ];
 
 export default function App() {
