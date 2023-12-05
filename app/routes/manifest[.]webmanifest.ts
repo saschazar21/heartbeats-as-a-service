6d912f65e7f3111154de22e9f8fe1169c2afacf9
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2023-12-05 12:27:19 +0100

    chore: added screenshots

--- app/routes/manifest[.]webmanifest.ts
+++ app/routes/manifest[.]webmanifest.ts
@@ -11,16 +11,58 @@ export const manifest = {
   description: pkg.description,
   icons: [
     {
-      src: "/android-chrome-192x192.png",
+      src: "/android-chrome-maskable-192x192.png",
       sizes: "192x192",
       type: "image/png",
       purpose: "maskable",
     },
     {
-      src: "/android-chrome-512x512.png",
+      src: "/android-chrome-maskable-512x512.png",
       sizes: "512x512",
       type: "image/png",
       purpose: "maskable",
+    },
+    {
+      src: "/android-chrome-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+      purpose: "any",
+    },
+    {
+      src: "/android-chrome-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+      purpose: "any",
+    },
+  ],
+  screenshots: [
+    {
+      form_factor: "narrow",
+      label: "Mobile light mode",
+      sizes: "750x1334",
+      src: "/screenshots/screenshot_mobile_light.png",
+      type: "image/png",
+    },
+    {
+      form_factor: "narrow",
+      label: "Mobile dark mode",
+      sizes: "750x1334",
+      src: "/screenshots/screenshot_mobile_dark.png",
+      type: "image/png",
+    },
+    {
+      form_factor: "wide",
+      label: "Desktop light mode",
+      sizes: "2160x1620",
+      src: "/screenshots/screenshot_desktop_light.png",
+      type: "image/png",
+    },
+    {
+      form_factor: "wide",
+      label: "Desktop dark mode",
+      sizes: "2160x1620",
+      src: "/screenshots/screenshot_desktop_dark.png",
+      type: "image/png",
     },
   ],
 };
